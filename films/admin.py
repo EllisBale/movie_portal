064ee08117fe4d1060b0bad44a1d7a346cb5ac47
EllisBale <elscar29@gmail.com>
2025-07-31 18:34:14 +0100

    Feat: add film and genre models

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Genre, Film
 # Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'release_date', 'is_popular')
+    list_filter = ('genre', 'is_popular', 'is_coming_soon', 'is_family')
+    search_fields = ('title', 'cast', 'description')
--- films/models.py
+++ films/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    poster = CloudinaryField('image', default='placeholder')
+    cast = models.TextField(
+        help_text="Comma-separated list of actors"
+    )
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    release_date = models.DateField(null=False, blank=False)
+    duration = models.PositiveIntegerField(
+        help_text="Total duration in minutes",
+        null=False, blank=False
+    )
+    is_popular = models.BooleanField(default=False)
+    is_coming_soon = models.BooleanField(default=False)
+    is_family = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+    def get_duration_display(self):
+        hours = self.duration // 60
+        minutes = self.duration % 60
+        if hours:
+            return f"{hours}h {minutes}m"
+        return f"{minutes}m"
