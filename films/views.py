b03a19f9dc7d0b4ea919a53fb3d8e940cf59a3f0
EllisBale <elscar29@gmail.com>
2025-08-05 16:11:41 +0100

    Refactor: update films.html layout to show all films

--- films/views.py
+++ films/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Genre
+from .models import Film
 
 # Create your views here.
 
 def film_list(request):
-    genres = Genre.objects.prefetch_related('film_set')
-    return render(request, 'films.html', {'genres': genres})
+    films = Film.objects.all()
+    return render(request, 'films.html', {'films': films})
+
