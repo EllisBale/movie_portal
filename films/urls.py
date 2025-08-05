dfbae47aa3e6c5d04087bcf23df998e07f718322
EllisBale <elscar29@gmail.com>
2025-08-05 16:50:17 +0100

    Feat: add url path and film_detail view function for films

--- films/urls.py
+++ films/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import film_list
+from .views import film_list, film_detail
 
 urlpatterns = [
     path('', film_list, name='films'),
+    path('<int:film_id>/', film_detail, name='film_detail'),
 ]
--- films/views.py
+++ films/views.py
@@ -7,3 +7,6 @@ def film_list(request):
     films = Film.objects.all()
     return render(request, 'films.html', {'films': films})
 
+def film_detail(request, film_id):
+    film = get_object_or_404(Film, id=film_id)
+    return render(request, 'films_detail.html', {'film': film})
