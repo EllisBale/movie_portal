cf99979efc692de1a8bcc925db20ea446a915865
EllisBale <elscar29@gmail.com>
2025-08-11 00:44:30 +0100

    Chore: collectstaticfiles for card hover effect

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -5,9 +5,31 @@ const buttonToTop = document.getElementById(
     "btn-to-top" // Back to top button
 ); 
 
+const posterHover = document.querySelectorAll(
+    '.card-poster-hover' // Poster hover
+);
 
 
 // Back to top function
 buttonToTop.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth"  }) 
 })
+
+
+// Hover effect for poster
+posterHover.forEach(poster => {
+    poster.addEventListener('mouseenter', () => {
+        posterHover.forEach(other => {
+            if (other !== poster) {
+                other.classList.add('inactive');
+            }
+        });
+    });
+
+    poster.addEventListener('mouseleave', () => {
+        posterHover.forEach(other => {
+            other.classList.remove('inactive');
+        });
+    });
+
+});
