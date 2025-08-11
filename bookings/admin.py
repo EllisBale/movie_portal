bbdff69c34aaceac3d34dd636e76cc92593c6600
EllisBale <elscar29@gmail.com>
2025-08-11 10:11:45 +0100

    Chore: remove booking migrations

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
-from .models import Booking, BookingSeat
 # Register your models here.
 
-@admin.register(Booking)
-class BookingAdmin(admin.ModelAdmin):
-    list_display = ('user', 'film', 'guests', 'date_booked',)
-    list_filter = ('user', 'guests', 'date_booked', 'film')
-    search_fields = ('date_booked', 'user', 'film')
-    ordering = ('-date', 'date_booked')
-
-@admin.register(BookingSeat)
-class BookingSeatAdmin(admin.ModelAdmin):
-    list_display = ('booking', 'row', 'number')
-    list_filter = ('row', 'booking')
-    search_fields = ('booking__user__username', 'booking__film__title')
--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
-from .forms import 
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
 
 
+def Bookings(request):
+    return HttpResponse("Test")
