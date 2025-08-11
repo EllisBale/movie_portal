8fcd3112d3cb78ec3326289891a0a80111869843
EllisBale <elscar29@gmail.com>
2025-08-11 13:12:15 +0100

    Feat: add book_seat function to views.py

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import BookingForm
 # Create your views here.
 
+@login_required
+def book_seat(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.save()
+            return redirect('booking_success')
+    else:
+        form = BookingForm()
+    return render(request, 'booings.html', {'form': form})
 
-def Bookings(request):
-    return HttpResponse("Test")
+
