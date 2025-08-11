b174fd31715197c9b9a17790fc6838d0df92943c
EllisBale <elscar29@gmail.com>
2025-08-11 09:50:42 +0100

    Chore: remove bookings model and form

--- bookings/views.py
+++ bookings/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import BookingForm
+from .forms import 
 # Create your views here.
 
 
-def Bookings(request):
-    if request.method == 'POST':
-        form = BookingForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-            form = BookingForm()
-
-    return render(request, 'bookings.html', {'form': form})
