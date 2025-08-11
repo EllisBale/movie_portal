d1ea3fb0bdedcc930e7f1f9a98843030bfbc27de
EllisBale <elscar29@gmail.com>
2025-08-11 02:53:51 +0100

    Feat: add booking model to booking app

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Booking
 # Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'film', 'guests', 'date_booked',)
+    list_filter = ('user', 'guests', 'date_booked', 'film')
+    search_fields = ('date_booked', 'user', 'film')
+
+    
--- bookings/models.py
+++ bookings/models.py
@@ -1,7 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from films.models import Film
+
 # Create your models here.
 
+def validate_not_past_date(current_date):
+    if current_date < timezone.localdate():
+        raise ValidationError("Booking date cannot be in the past.")
+    
+
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    """Dynamically generate times between 10am and midnight""" 
+
+    TIME_CHOICES = [
+        (f"{h}am", f"{h}am") if h < 12 else
+        (f"{h - 12 if h > 12 else 12}pm", f"{h - 12 if h > 12 else 12}pm")
+        for h in range(10, 24)
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, default=1)
+    date = models.DateField(default=timezone.now, validators=[validate_not_past_date])
+    time = models.CharField(max_length=5, choices=TIME_CHOICES, default="10am")
+    guests = models.PositiveIntegerField(default=1)
+    date_booked = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['date', 'user']
+
+    def __str__(self):
+        return f"Booking for {self.film} on {self.date} at {self.time} by {self.user}"
     
