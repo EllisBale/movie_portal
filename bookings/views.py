db4f365c9a2afce97faa7357066073a23bd03361
EllisBale <elscar29@gmail.com>
2025-08-12 21:59:30 +0100

    Feat: add view for form

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from films.models import FilmSchedule
+from .forms import BookingForm
+from .models import Booking
 
 
-def test(request):
-    return HttpResponse
+def book_film_seat(request, schedule_id):
+    schedule = get_object_or_404(FilmSchedule, id=schedule_id)
+
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            seats = form.cleaned_data['seat_numbers']
+
+            for seat in seats:
+                Booking.objects.create(
+                    schedule=schedule,
+                    seat_number=seat,
+                    user=request.user
+                )
+            return redirect('bookings_success')
+    else:
+        form = BookingForm()
+
+    return render(request, 'bookings/book_seats.html', {'form': form, 'schedule': schedule})
