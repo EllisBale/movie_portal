807f6b957fca4d3915f1ec6ca0b59d6676b05c73
EllisBale <elscar29@gmail.com>
2025-08-12 22:26:37 +0100

    Feat: connect template for book_seats.html

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import book_film_seat, booking_success
 
 urlpatterns = [
-    path('', book_film_seat, name='bookings'),
+    path('', book_film_seat, name='book_film_seat'),
     path('success/', booking_success, name='bookings_success'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -5,26 +5,23 @@ from .forms import BookingForm
 from .models import Booking
 
 
-def book_film_seat(request, schedule_id):
-    schedule = get_object_or_404(FilmSchedule, id=schedule_id)
+def book_film_seat(request):
+
+    schedule = FilmSchedule.objects.first()
+    if not schedule:
+        return render(request, '404.html')
 
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
             seats = form.cleaned_data['seat_numbers']
-
             for seat in seats:
-                Booking.objects.create(
-                    schedule=schedule,
-                    seat_number=seat,
-                    user=request.user
-                )
+                Booking.objects.create(schedule=schedule, seat_number=seat, user=request.user)
             return redirect('bookings_success')
     else:
         form = BookingForm()
 
-    return render(request, 'bookings/book_seats.html', {'form': form, 'schedule': schedule})
-
+    return render(request, 'book_seats.html', {'form': form, 'schedule': schedule})
 
 def booking_success(request):
     return render(request, 'bookings_success.html')
