63a43ebbb59f94d0c7d3debfdee9294c7cf42421
EllisBale <elscar29@gmail.com>
2025-08-18 14:49:31 +0100

    Feat: import film schedule to films app for films_detail.html

--- films/urls.py
+++ films/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import film_list, film_detail
-
+from bookings.views import film_schedules
 urlpatterns = [
     path('', film_list, name='films'),
     path('<int:film_id>/', film_detail, name='film_detail'),
+    path('schedules/<int:film_id>/',  film_schedules, name='film_schedules')
 ]
