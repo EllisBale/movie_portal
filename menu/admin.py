8e43df11a649f24b43f7443ff82ea25081725849
EllisBale <elscar29@gmail.com>
2025-08-19 13:19:51 +0100

    Feat: add menu model and admin model to menu app

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Menu
 # Register your models here.
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'description')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+    ordering = ('category', 'name')
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Menu(models.Model):
+    CATEGORY_CHOICES = [
+        ('food', 'Food'),
+        ('drink', 'Drink'),
+    ]
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    image = CloudinaryField('image', default='placeholder')
+    category = models.CharField(
+        max_length=10,
+        choices=CATEGORY_CHOICES
+    )
+
+    class Meta:
+        ordering = ['category', 'name']
+
+    def __str__(self):
+        return self.name
