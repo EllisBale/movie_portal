a54f9b7e2df243267ddf0a9782ad66e803538305
EllisBale <elscar29@gmail.com>
2025-08-26 16:23:09 +0100

    Feat: add CRUD urls to films url.py

--- films/urls.py
+++ films/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import film_list, film_detail
+from .views import film_list, film_detail, film_create, film_delete, film_update
 from bookings.views import film_schedules
+
 urlpatterns = [
     path('', film_list, name='films'),
     path('<int:film_id>/', film_detail, name='film_detail'),
-    path('schedules/<int:film_id>/',  film_schedules, name='film_schedules')
+    path('schedules/<int:film_id>/',  film_schedules, name='film_schedules'),
+    path('create/',  film_create, name='film_create'),
+    path('delete/<int:pk/',  film_delete, name='film_delete'),
+    path('update/<int:pk/',  film_update, name='film_update'),
 ]
