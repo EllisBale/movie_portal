c544f9024f4e34ce613d976c9e984fc0ea51eb1c
EllisBale <elscar29@gmail.com>
2025-08-28 22:28:03 +0100

    Feat: add password_reset_done page

--- movie_portal/settings.py
+++ movie_portal/settings.py
@@ -65,8 +65,9 @@ INSTALLED_APPS = [
 
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',   
+    'django.contrib.auth.backends.ModelBackend'
+    
 ]
 
 SITE_ID = 1
