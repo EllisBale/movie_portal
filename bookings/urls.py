b7a3d7b2b5f27237d0e7260a7f17deb395c137b9
EllisBale <elscar29@gmail.com>
2025-09-05 16:10:24 +0100

    Feat: add user_booking.html for logged in users

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import booking_page, select_film, booking_success, booking_page, film_schedules
+from .views import booking_page, select_film, booking_success, booking_page, film_schedules, user_booking
 
 
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('film/<int:film_id>/schedules/', film_schedules, name='film_schedules'),
     path('<int:schedule_id>/', booking_page, name='booking_page'),
     path('success/', booking_success, name='booking_success'),
+    path('my_bookings/', user_booking, name='user_bookings'),
 ]
