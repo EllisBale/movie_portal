22a201f443d89f04587099c806bb7fcbeef05430
EllisBale <elscar29@gmail.com>
2025-09-09 19:04:53 +0100

    Fix: add required to first name, last name in profile

--- user_account/forms.py
+++ user_account/forms.py
@@ -19,12 +19,26 @@ class CustomSignupForm(SignupForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=50, required=True, label='First Name')
+    last_name = forms.CharField(max_length=50, required=True, label="Last Name")
     email = forms.EmailField(required=True, label='Email Address')
 
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email"]
-
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if not first_name.strip():
+            raise forms.ValidationError("First name is required.")
+        return first_name
+    
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if not last_name.strip():
+            raise forms.ValidationError("Last name is required.")
+        return last_name
+    
     def clean_email(self):
         email = self.cleaned_data.get('email')
 
