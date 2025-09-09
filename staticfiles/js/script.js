711ed99d1726f0b3215a97bdd979afec1e23192c
EllisBale <elscar29@gmail.com>
2025-09-09 02:44:18 +0100

    Feat: add modal for book_seats.html

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -5,6 +5,9 @@ const buttonToTop = document.getElementById("btn-to-top"); // Back to top button
 const posterHover = document.querySelectorAll(".card-poster-hover"); // Poster hover
 const bookSeatForm = document.getElementById("book_seats_form"); // Book seats form
 const deleteBtns = document.querySelectorAll(".delete-button"); // Delete button
+const customModal = document.getElementById("customModal"); // Modal
+const customModalBody = document.getElementById("customModalBody"); // Modal body
+const customModalClose = document.getElementById("customModalClose"); // Modal close
 
 
 /* ===== Back to Top ===== */
@@ -35,11 +38,24 @@ posterHover.forEach(poster => {
 bookSeatForm?.addEventListener("submit", (event) => {
 
     const selectedSeats = document.querySelectorAll(".seat-checkbox:checked");
+    event.preventDefault();
 
     if (selectedSeats.length === 0) {
-        event.preventDefault();
-        alert(" Select at least one seat to book.");
+        customModalBody.textContent = "Select at least one seat to book.";
+        customModal.style.display = "flex";
+    
+    } else if (selectedSeats.length > 8) {
+        customModalBody.textContent = "You can only select a maximum of 8 seats.";
+        customModal.style.display = "flex";
+    } else {
+        bookSeatForm.submit();
     }
+
+});
+
+
+customModalClose?.addEventListener("click", () => {
+    customModal.style.display = "none";
 });
 
 
@@ -51,4 +67,3 @@ deleteBtns.forEach(deleteBtn => {
         }
     });
 });
-
