25f20e2dc48983fb762c3354c4c9947b94de3ca5
EllisBale <elscar29@gmail.com>
2025-09-13 15:18:35 +0100

    Feat: add username to UserForm for management

--- management/forms.py
+++ management/forms.py
@@ -39,6 +39,7 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
+            'username',
             'first_name',
             'last_name',
             'email',
